05ff544a37495879b0cdbd97a7bf2eabec5fc859
angels101 <angelscodex101@gmail.com>
2021-09-19 17:28:30 +0300

    if fucntion

--- myapp/app.py
+++ myapp/app.py
@@ -3,11 +3,18 @@ from flask import Flask, request,render_template
 
 app=Flask(__name__)
 
-@app.route("/calculate")
+@app.route("/", methods=['POST', 'GET'])
 
-def calculate():
+def hello_world():
     bmi=''
-    Weight=float(request.form.get('weight'))
-    Height=float(request.form.get('weight'))
 
-    return render_template("index.html")
+    if request.method=='POST' and 'weight' in request.form and 'height' in request.form:
+
+        
+        Weight=float(request.form.get('weight'))
+
+        Height=float(request.form.get('height'))
+
+        bmi= round(Weight/((Height/100)**2),2)
+
+    return render_template('index.html',bmi=bmi)
